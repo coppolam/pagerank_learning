4dc93919f22c15556b185fc0a672b3adce2ebba9
coppolam <m.coppola@tudelft.nl>
2019-07-06 13:58:44 +0200

    [checkpoint] demo video

--- aggregation_ga.py
+++ aggregation_ga.py
@@ -16,7 +16,7 @@ import matplotlib as plot
 from pybrain.optimization import GA
 
 cpp_run = True
-nn = 2
+nn = 1
 graph = True
 import_mat_data = True
 M = 8
@@ -33,16 +33,17 @@ if import_mat_data:
     E = np.asmatrix(np.reshape(np.loadtxt(folder+"inputs/E_"+str(nn)+".in"),(M,M)))
 else:
     # Test
+#    H = np.matlib.ones((M,M))
+#    E = np.matlib.eye(M)
     alpha = np.matlib.eye(M,M) * 0.5
     H = np.matlib.ones((M,M)) * 0.2
-    E = np.matlib.ones((M,M)) * 0.2
 
 def normalize_rows(mat):
     row_sums = np.squeeze(np.asarray(mat.sum(axis=1)))
     mat_norm = mat / row_sums[:, np.newaxis]
     return mat_norm
 
-def pagerank(G, tol=1e-5):
+def pagerank(G, tol=1e-6):
     # Iterative procedure
     n = G.shape[0] # Size of G
     pr = 1 / n * np.ones((1, n)) # Initialize Pagerank vector
@@ -51,7 +52,6 @@ def pagerank(G, tol=1e-5):
         pr_previous = pr
         pr = np.matmul(pr,G) # Pagerank formula
         residual = np.linalg.norm(np.subtract(pr,pr_previous))
-#        print(residual)
     return np.squeeze(np.asarray(pr))
 
 def fitness_function(pr):
@@ -61,7 +61,7 @@ def fitness_function(pr):
 def objF(x):
     aM = np.asmatrix(np.diag(alpha)) # Alpha vector in matrix format
     Hm = np.asmatrix(np.asarray(H) * x[:, np.newaxis]) # Multiply probability of transition with probability of policy
-    z = np.where(x <= 0.001)[0] # Find transitions with no action
+    z = np.where(x <= 0.00001)[0] # Find transitions with no action
     D = np.matlib.zeros((M,M)) # Blank slate for matrix D 
     D[z,:] = E[z,:] # Account for blocked states in matrix D
     S = normalize_rows(Hm + D) # Normalize policy matrix S = H + D
@@ -69,6 +69,8 @@ def objF(x):
     G = np.matmul(aM, S) + np.matmul(np.subtract(np.eye(aM.shape[0]), aM), Em)
     pr = pagerank(G) # Evaluate pagerank vector 
     fitness = fitness_function(pr) # Get fitness
+    
+#    print(fitness)
     return fitness
 
 def extract_history(l):
@@ -91,6 +93,10 @@ def initialize_evolution_parameters(l):
 x0 = np.ones(M)/2 # Initialize to ones
 GA.xBound = list(zip(list(np.zeros(M)),list(np.ones(M)))) # Set limits
 GA.elitism = True # Use elite mem
+#GA.eliteProportion = 0.1
+GA.mutationProb = 0.5
+GA.verbose = True
+GA.mutationStdDev = 0.2
 l = GA(objF, x0) # Set up GA (alternative subclass)
 l = initialize_evolution_parameters(l)
 
