daecc7d600109becf77237014f05dd8cb3d736d4
Mario Coppola <m.coppola@tudelft.nl>
2020-03-17 20:21:38 +0100

    cleanup

--- main_graphless.py
+++ main_graphless.py
@@ -2,30 +2,30 @@ import numpy as np
 import fileHandler as fh
 import matrixOperations as matOp
 import scipy.optimize as spopt
-
 import os
 import subprocess
-import networkx as nx
 
+np.set_printoptions(suppress=True) # Avoid scientific notation
+		
 pol_sim = np.array([0.5,0.5,0.5,0.5,0.5,0.5,0.5])
 des = np.array([2,3,4,5,6])
-verbose = 1
+verbose = 1 #0 barely, 1 = some, 2 = a lot
 
 def fitness(pr,des):
 	return np.mean(pr[:,des])/np.mean(pr)
 
-def objective_function(pol,alpha,H,E,A):
+def objective_function(pol, alpha, H, E, A):
 	Hnew = matOp.update_H(H, A, E, pol_sim, pol) # Update H
 	G = np.diag(alpha).dot(Hnew) + np.diag(1-alpha).dot(E)
 	pr = matOp.pagerank(G)
 	f = fitness(pr,des)
 	if verbose > 1:
-		print(str(round(pol[0],4)) + 
+		print(str(round(pol[0],5)) + 
 			" Fitness \tf = " + str(round(f,5)) + 
-			"\t1/(1+f) = " + str(round(1/(f+1),5)))
+			"\t1/(1+f) = " + str(round(1/(f + 1),5)))
 	return 1 / (f + 1) # Trick it into maximizing
 
-def optimize(alpha,H, E, A):
+def optimize(alpha, H, E, A):
 	s = "randtobest1bin"
 	# s = "best2bin"
 	# s = "best2exp"
@@ -66,7 +66,6 @@ def main():
 		
 		# Read output of simulation
 		H, A, E = fh.read_matrices("../swarmulator/mat/")
-		np.set_printoptions(suppress=True)
 		r = np.sum(H, axis=1) / np.sum(E, axis=1)
 		r = np.nan_to_num(r) # Just in case
 		alpha = r / (1 + r)
@@ -76,7 +75,7 @@ def main():
 			print(E)
 			print(alpha)
 
-		policy, fitness = optimize(alpha,H,E,A)
+		policy, fitness = optimize(alpha, H, E, A)
 
 if __name__ == '__main__':
 	main()
