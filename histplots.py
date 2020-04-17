f1fc52df37727863acee48788e180060f243cca4
Mario Coppola <m.coppola@tudelft.nl>
2020-04-17 19:35:11 +0200

    added unique pipe id so you can launch many sims at once

--- histplots.py
+++ histplots.py
@@ -8,34 +8,52 @@ import numpy as np
 from tools import fileHandler as fh
 import matplotlib
 import matplotlib.pyplot as plt
+import sys
 
 #### Input
 folder = "data/"
-file  = "2020_04_13_18:07:16"
+file  = "2020_04_17_19:11:43"
 alpha = 0.5;
 matplotlib.rcParams['text.usetex'] = False # True for latex style
-plot = False
+plot = True
 #### Load validation data
 data_validation = np.load(folder+file+"_validation.npz")
-fitness_0 = data_validation['arr_0'].astype(float)
-fitness_n = data_validation['arr_1'].astype(float)
-
+fitness_0 = data_validation['f_0'].astype(float)
+fitness_n = data_validation['f_n'].astype(float)
+#data_validation_05 = np.load(folder+"2020_04_16_11:56:28_validation_0.5.npz")
+#data_validation_bt = np.load(folder+"2020_04_16_14:15:29_validation_behaviortree.npz")
+#fitness_n2 = data_validation_05['f_n'].astype(float)
+#fitness_n3 = data_validation_bt['f_n'].astype(float)
 #### Plot validation data
 if plot:
     plt.hist(fitness_0, alpha=alpha, density=True, label='$\pi_0$')  # arguments are passed to np.histogram
     plt.hist(fitness_n, alpha=alpha, density=True, label='$\pi_n$')  # arguments are passed to np.histogram
+#    plt.hist(fitness_n2,alpha=alpha, density=True, label='$\pi_n0.5$')
+#    plt.hist(fitness_n3,alpha=alpha, density=True, label='$\pi_nbt$')
     plt.xlabel("Fitness")
     plt.ylabel("Instances")
     plt.legend(loc='upper right')
     plt.show()
 
 #### Load optimization data
+np.set_printoptions(threshold=sys.maxsize,suppress=True)
 data_policy = np.load(folder+file+"_optimization.npz")
-des = data_policy['arr_0'].astype(float)
-policy = data_policy['arr_2'].astype(float)
-H = data_policy['arr_3'].astype(float)
-A = data_policy['arr_4'].astype(float)
-E = data_policy['arr_5'].astype(float)
+des = data_policy['des'].astype(float)
+policy = data_policy['policy'].astype(float)
+H = data_policy['H'].astype(float)
+A = data_policy['A'].astype(float)
+E = data_policy['E'].astype(float)
+
+print("H = ")
+print(H)
+print("E = ")
+print(E)
+print("A = ")
+print(A)
+print("des = ")
+print(des)
+print("policy = ")
+print(policy)
 
 #### Investigate policy behavior
 ###### Simulate ######
--- main_aggregation.py
+++ main_aggregation.py
@@ -9,36 +9,51 @@ from tools import fileHandler as fh
 from tools import matrixOperations as matop
 import numpy as np
 import matplotlib.pyplot as plt
-import subprocess, sys, shutil, os
+import subprocess, sys
 import datetime
+
+## Options
+rerun = True
+evaluate = False
+n = 30 # Robots
 
 ###### Simulate ######
 from simulator import swarmulator
-rerun = True
-evaluate = True
-n = 30 # Robots
 save_id = "data/" + str(datetime.datetime.now().strftime("%Y_%m_%d_%H:%M:%S"))
 folder = "../swarmulator"
 data_folder = folder + "/logs/"
 sim = swarmulator.swarmulator(folder) # Initialize
-sim.runtime_setting("simulation_realtimefactor", "50")
-sim.runtime_setting("time_limit", "2000")
-sim.runtime_setting("environment", "square")
+sim.runtime_setting("time_limit", "100")
 
 if rerun:
+	sim.runtime_setting("simulation_realtimefactor", "50")
+	sim.runtime_setting("environment", "square")
 	sim.make(clean=True,animation=False,logger=False,verbose=True) # Build (if already built, you can skip this)
 	sim.runtime_setting("policy", "") # Use random policy
 	subprocess.call("cd " + data_folder + " && rm *.csv", shell=True)
 	f = sim.run(n) # Run it, and receive the fitness.
 
+print("done")
 ###### Optimize ######
 H = fh.read_matrix(data_folder,"H")
 A = fh.read_matrix(data_folder,"A")
 E = fh.read_matrix(data_folder,"E")
 des = fh.read_matrix(data_folder,"des")
+np.savez(save_id+"_estimated_data", des=des, H=H, A=A, E=E)
 policy = np.ones([A.shape[1],int(A.max())]) / 2
 result, policy, empty_states = opt.main(policy, des, H, A, E)
 np.savez(save_id+"_optimization", des=des, policy=policy, H=H, A=A, E=E)
+#data = np.load('data/2020_04_16_10:58:24_optimization.npz')
+#H = data['H'].astype(float)
+#E = data['E'].astype(float)
+#A = data['A'].astype(float)
+#policy = np.ones([A.shape[1],int(A.max())]) /2 
+#des = data['des'].astype(float)
+#prl = np.sum(H,axis=1)+np.sum(E,axis=1)
+#Hprl = matop.normalize_rows(H/prl)
+#Eprl = matop.normalize_rows(E/prl)
+#desprl = matop.normalize_rows(des/prl,axis=0)
+#result, policy, empty_states = opt.main(policy,des,Hprl,A,Eprl)
 
 ###### Print results to terminal ######
 print(H)
@@ -56,21 +71,25 @@ print(policy)
 if evaluate:
 	runs = 100
 	sim.runtime_setting("time_limit", "1000")
+	sim.runtime_setting("simulation_realtimefactor", "50")
+	sim.runtime_setting("environment", "square")
 
 	# Benchmark #
 	f_0 = []
+	sim.runtime_setting("policy", "") # Use random policy
 	for i in range(0,runs):
 		print('{:=^40}'.format(' Simulator run '))
-		sim.runtime_setting("policy", "") # Use random policy
+		print("Run " + str(i) + "/" + str(runs))
 		f_0 = np.append(f_0,sim.run(n))
 
 	# Optimized #
 	f_n = []
 	policy_file = sim.path + "/conf/state_action_matrices/aggregation_policy.txt"
 	fh.save_to_txt(policy.T, policy_file)
+	sim.runtime_setting("policy", policy_file) # Use random policy
 	for i in range(0,runs):
 		print('{:=^40}'.format(' Simulator run '))
-		sim.runtime_setting("policy", policy_file) # Use random policy
+		print("Run " + str(i) + "/"  +str(runs))
 		f_n = np.append(f_n,sim.run(n))
 
 	np.savez(save_id+"_validation", f_0=f_0, f_n=f_n)
