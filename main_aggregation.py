f5147a155b298d85212c33bf92032272fd85e0b2
Mario Coppola <m.coppola@tudelft.nl>
2020-04-22 23:15:26 +0200

    Added fitness functions

--- main_aggregation.py
+++ main_aggregation.py
@@ -7,6 +7,9 @@ Simulate the aggregation and optimize the behavior
 import aggregation, sys
 import numpy as np
 rerun = False
+import matplotlib
+import matplotlib.pyplot as plt
+import networkx 
 
 sim = aggregation.aggregation()
 
@@ -17,22 +20,47 @@ else:
 	sim.load(sys.argv)
 	# sim.sim.plot_log()
 
-# distance_to_others()
+def evaluate_fitness(data):
+	id_column = 1
+	robots = int(data[:,id_column].max())
+	p = data[0:robots,(2,3)] # Positions
+	rangesensor = 1.8
+	f = 0
 
-def evaluate_fitness(d):
-    # for r in sim.robots
-	p = d[:,(2,3)]
-	for r in range(0,sim.robots)
-		p[r,:]-p
-	return fitness
+	# Clustering coefficient
+	A = np.zeros([robots,robots]) # Adjacency matrix
+	for r in range(0,robots):
+		p_rel = p[r,:]-p
+		d = (np.sqrt(p_rel[:,0]**2+p_rel[:,1]**2) < rangesensor)
+		A[r,:] = d
+	G = networkx.from_numpy_array(A)
+	f = networkx.average_clustering(G)
+
+	# Mean number of neighbors
+	# for r in range(0,robots):
+	# 	p_rel = p[r,:]-p
+	# 	d = np.where((np.sqrt(p_rel[:,0]**2+p_rel[:,1]**2) < rangesensor))[0]
+	# 	n_neighbors = d.size
+	# 	f += (n_neighbors-1) / robots
+
+	# Mean distance to neighbors
+	# for r in range(0,robots):
+	# 	p_rel = p[r,:]-p
+	# 	d = np.sqrt(p_rel[:,0]**2+p_rel[:,1]**2)
+	# 	f += d.mean()/robots
+	return f
 
 time_column = 0
 t = np.unique(sim.data[:,0])
 fitness = np.zeros(t.shape)
+a = 0
 for step in t:
 	d = sim.data[np.where(sim.data[:,time_column] == step)]
-	fitness = evaluate_fitness(d)
+	fitness[a] = evaluate_fitness(d)
+	a += 1
 
+plt.plot(np.arange(0,fitness.size),fitness)
+plt.show()
 # sim.optimize()
 # sim.evaluate(runs=1)
 # sim.histplots()
