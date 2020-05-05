77faa789e1f15dcad10d93c8b9a0f9b06b79f465
Mario Coppola <m.coppola@tudelft.nl>
2020-05-05 23:06:35 +0200

    fixed filename of checkpoint

--- main_standard_evolution.py
+++ main_standard_evolution.py
@@ -12,9 +12,11 @@ sim.make(controller=c, agent=a, clean=True, logger=False, verbose=False)
 sim.runtime_setting("time_limit", str("200"))
 sim.runtime_setting("simulation_realtimefactor", str("50"))
 sim.runtime_setting("environment", "square")
+filename = "evo_run_%s_%s" % (c, a)
 
 def fitness(individual):
 	policy_file = "../swarmulator/conf/state_action_matrices/policy_evolved_temp.txt"
+	individual = np.reshape(individual,(16,8))
 	fh.save_to_txt(individual, policy_file)
 	sim.runtime_setting("policy", policy_file) # Use random policy
 	f = sim.batch_run((10,30),5)
@@ -22,6 +24,6 @@ def fitness(individual):
 	return f.mean(), # comma to cast to tuple!
 
 e = evolution.evolution()
-e.setup(fitness, GENOME_LENGTH=8, POPULATION_SIZE=100)
-p = e.evolve(verbose=True, generations=100, checkpoint=True)
-e.save("evo_run_%s_%s" % (c, a))
+e.setup(fitness, GENOME_LENGTH=16*8, POPULATION_SIZE=100)
+p = e.evolve(verbose=True, generations=100, checkpoint=filename)
+e.save(filename)
