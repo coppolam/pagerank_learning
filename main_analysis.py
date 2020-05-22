edfa3d8c640dd96b6121775e692ac087f2c61897
Mario Coppola <m.coppola@tudelft.nl>
2020-05-22 18:59:33 +0200

    restored normal main_analysis

--- main_analysis.py
+++ main_analysis.py
@@ -100,12 +100,12 @@ def benchmark(file,time_limit=100):
 	# e.load(folder+"evolution")
 	# p_s = e.get_best()
 	# p_s = np.reshape(p_s,(16,8))
-	# f_0 = sim.benchmark(p_0,args.controller,args.agent,fitness,runs=100,time_limit=time_limit)
-	# f_n = sim.benchmark(p_n,args.controller,args.agent,fitness,runs=100,time_limit=time_limit)
-	sim.observe(p_n,args.controller,args.agent)
+	f_0 = sim.benchmark(p_0,args.controller,args.agent,fitness,runs=100,time_limit=time_limit)
+	f_n = sim.benchmark(p_n,args.controller,args.agent,fitness,runs=100,time_limit=time_limit)
+	# sim.observe(p_n,args.controller,args.agent)
 	# f_s = sim.benchmark(p_s,args.controller,args.agent,time_limit=time_limit)
 	# data_validation = np.savez(folder + "benchmark.npz",f_0=f_0,f_n=f_n,f_s=f_s,p_0=p_0,p_n=p_n,p_s=p_s)
-	# data_validation = np.savez(folder + "benchmark_%s"%file,f_0=f_0,f_n=f_n,p_0=p_0,p_n=p_n)
+	data_validation = np.savez(folder + "benchmark_%s"%file,f_0=f_0,f_n=f_n,p_0=p_0,p_n=p_n)
 
 def plot_benchmark(file):
 	data = np.load(folder + "benchmark_%s"%file)
