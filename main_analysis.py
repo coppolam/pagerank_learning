9e87460e10660e194369c29be204c58123becfec
Mario Coppola <m.coppola@tudelft.nl>
2020-06-03 19:58:17 +0200

    clean

--- main_analysis.py
+++ main_analysis.py
@@ -72,44 +72,36 @@ def optimize(file,p0,des):
 	return sim.optimize(p0,des)
 
 def benchmark(file,time_limit=100):
+    # reference
 	if args.controller == "aggregation":
 		fitness = "aggregation_clusters"
 		p_0 = np.ones((8,1))/2 # all = 1/2
-		# des = np.zeros([1,8])[0]
-		# des[4:] = 1 # from nn
 	elif args.controller == "pfsm_exploration":
 		fitness = "aggregation_clusters"
 		p_0 = np.ones((16,8))/8 # all = 1/8
-		# des = np.ones([1,16])[0]
-		# des[0] = 0 # from nn
 	elif args.controller == "pfsm_exploration_mod":
 		fitness = "aggregation_clusters"
-		p_0 = np.ones((16,8))/8 # all = 1/8
-		# des = np.ones([1,16])[0]
-		# des[0] = 0 # from nn
-		# des = [0.0, 0.9848900061808971, 0.976980500207466, 0.9891087896631704, 0.980456436324312, 0.9935526779444446, 0.9865328989055827, 0.991669223914925, 0.9754366280896926, 0.9634689362627317, 0.9923385484054741, 0.9995649232133225, 0.9128809930185515, 0.9990481518590899, 0.9847521337659593, 0.9949568797324759]
+		p_0 = np.ones((16,8))/8
 	elif args.controller == "forage":
 		fitness = "food"
-		p_0 = np.ones((16,1))/2
-		# des = [0.9930061658199832, 0.9952269357749232, 0.9701479122355184, 0.9180163981328413, 0.9953555881858656, 0.9958821520830611, 0.9850341184365603, 0.9597566822246582, 1.0, 1.0, 0.9812966712145966, 0.9955001082675093, 0.0, 0.9988234867265866, 0.0, 0.0]
-		# des = [0.9924909975428292, 0.995950167064943, 1.0, 1.0, 0.9917787816675288, 0.9977750734814962, 0.9539859077977527, 0.9987997668920423, 0.0, 0.9990176940934485, 0.9865668167518789, 0.0, 0.0, 0.0, 0.0, 0.0]
+		p_0 = np.ones((16,1))/2	
 	else:
 		ValueError("Uknown inputs!")
 
 	des = desired_states_extractor.desired_states_extractor().run(file,verbose=True)
 	p_n = optimize(file, p_0, des)
+	f_0 = sim.benchmark(p_0,args.controller,args.agent,fitness,runs=100,time_limit=time_limit)
+	f_n = sim.benchmark(p_n,args.controller,args.agent,fitness,runs=100,time_limit=time_limit)
+	sim.observe(p_n,args.controller,args.agent)
+	data_validation = np.savez(folder + "benchmark_%s"%file,f_0=f_0,f_n=f_n,p_0=p_0,p_n=p_n)
 
 	# e = evolution.evolution()
 	# e.load(folder+"evolution")
 	# p_s = e.get_best()
 	# p_s = np.reshape(p_s,(16,8))
-	# f_0 = sim.benchmark(p_0,args.controller,args.agent,fitness,runs=100,time_limit=time_limit)
-	# f_n = sim.benchmark(p_n,args.controller,args.agent,fitness,runs=100,time_limit=time_limit)
-	sim.observe(p_n,args.controller,args.agent)
 	# f_s = sim.benchmark(p_s,args.controller,args.agent,time_limit=time_limit)
 	# data_validation = np.savez(folder + "benchmark.npz",f_0=f_0,f_n=f_n,f_s=f_s,p_0=p_0,p_n=p_n,p_s=p_s)
-	# data_validation = np.savez(folder + "benchmark_%s"%file,f_0=f_0,f_n=f_n,p_0=p_0,p_n=p_n)
-
+	
 def plot_benchmark(file):
 	data = np.load(folder + "benchmark_%s"%file)
 	alpha = 0.5
